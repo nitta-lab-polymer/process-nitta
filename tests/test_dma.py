7c4934399679a84d7eb3fab228e90d9711d0d727
Mkamono <chengyaye31@gmail.com>
2023-12-23 19:52:10 +0000

    Fix file path in test_dma.py

--- tests/test_dma.py
+++ tests/test_dma.py
@@ -7,7 +7,7 @@ from process_nitta import DMASample
 def test_dma():
     workspace_dir = Path(__file__).parent.parent
     sample = DMASample(
-        file_path=f"{workspace_dir}a/sample_data/dma.csv",
+        file_path=f"{workspace_dir}/sample_data/dma.csv",
         name="sample",
     )
     result_df = sample.get_result_df()
