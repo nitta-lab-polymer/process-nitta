9b412250beff0e5aba0bf62b82ff07f165815016
Mkamono <chengyaye31@gmail.com>
2023-12-23 19:47:58 +0000

    Fix file paths in test files

--- tests/test_dma.py
+++ tests/test_dma.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+
 import pandas as pd
 from process_nitta import DMASample
 
 
 def test_dma():
+    workspace_dir = Path(__file__).parent.parent
     sample = DMASample(
-        file_path="./process-nitta/sample_data/dma.csv",
+        file_path=f"{workspace_dir}a/sample_data/dma.csv",
         name="sample",
     )
     result_df = sample.get_result_df()
-    answer_df = pd.read_csv("./process-nitta/tests/answer_data/dma.csv", index_col=0)
+    answer_df = pd.read_csv(f"{workspace_dir}/tests/answer_data/dma.csv", index_col=0)
     pd.testing.assert_frame_equal(result_df, answer_df)
