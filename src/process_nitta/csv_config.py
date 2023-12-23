a74436eb62dc1627d8cf66a7ae61cd186908a6cf
Mkamono <chengyaye31@gmail.com>
2023-12-23 19:57:25 +0000

    Add 'engine' attribute to CSVConfig class

--- src/process_nitta/csv_config.py
+++ src/process_nitta/csv_config.py
@@ -45,6 +45,7 @@ class CSVConfig(BaseModel):
     skiprows: Optional[List[int]] = None  # 冒頭の行を読み飛ばす動作は許可しない
     skipfooter: int = 0
     nrows: Optional[int] = None
+    engine: str = "python"
 
     def to_dict(self) -> Dict[str, Any]:
         if sys.version_info < (3, 11):
