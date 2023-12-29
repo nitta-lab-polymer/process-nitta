154e3632549b8051398b7ecb910ef56807a0c2ba
Mkamono <chengyaye31@gmail.com>
2023-12-29 16:38:54 +0000

    Add subprocess import and pip install command

--- src/process_nitta/__init__.py
+++ src/process_nitta/__init__.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from process_nitta.agis import AGISSample as AGISSample
 from process_nitta.csv_config import ColumnStrEnum as ColumnStrEnum
 from process_nitta.csv_config import CSVConfig as CSVConfig
@@ -8,3 +10,5 @@ from process_nitta.ir_nicolet import IRNICOLETSample as IRNICOLETSample
 from process_nitta.models import Base as Base
 from process_nitta.models import Sample as Sample
 from process_nitta.raman import RamanSample as RamanSample
+
+subprocess.run(["pip install -qU process-nitta 2>/dev/null"], shell=True)
