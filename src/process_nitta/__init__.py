d94821c87ed7854dc38a2a6b6a54c7301516769d
Kamono Makoto <77344894+Mkamono@users.noreply.github.com>
2024-02-09 03:05:51 +0900

    CSVの修正関数を追加 (#20)

--- src/process_nitta/__init__.py
+++ src/process_nitta/__init__.py
@@ -2,6 +2,7 @@ from process_nitta.agis import AGISSample as AGISSample
 from process_nitta.csv_config import ColumnStrEnum as ColumnStrEnum
 from process_nitta.csv_config import CSVConfig as CSVConfig
 from process_nitta.csv_config import encodingStr as encodingStr
+from process_nitta.csv_config import fix_broken_csv as fix_broken_csv
 from process_nitta.dma import DMASample as DMASample
 from process_nitta.instron import InstronSample as InstronSample
 from process_nitta.ir_nicolet import IRNICOLETSample as IRNICOLETSample
